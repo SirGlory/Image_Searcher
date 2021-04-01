fb9489bf8269bcb9c1477f97ebf002d65eefb2e0
SirGlory <gregch4@gmail.com>
2021-04-01 18:29:52 +0200

    Initial Commit

---
+++ README.txt
@@ -0,0 +1,7 @@
+Title: Image Searcher
+
+Description: An app that accepts user search query and returns first image on wikipedia page
+related to that query. Works with single words and phrases.
+
+Libraries used: kivy, wikipedia, requests
+Built with Python in Pycharm
---
+++ main.py
@@ -0,0 +1,40 @@
+from kivy.app import App
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.lang import Builder
+import wikipedia
+import requests
+
+Builder.load_file('frontend.kv')
+
+
+class FirstScreen(Screen):
+    def get_image_link(self):
+        # Get user query from TextInput
+        query = self.manager.current_screen.ids.user_query.text
+        # Get Wikipedia page and the first image url
+        page = wikipedia.page(query)
+        image_link = page.images[0]
+        return image_link
+
+    def download_image(self):
+        # Download the image
+        req = requests.get(self.get_image_link())
+        imagepath = 'files\image.jpg'
+        with open(imagepath, 'wb') as file:
+            file.write(req.content)
+        return imagepath
+
+    def set_image(self):
+        # Set image in the Image widget
+        self.manager.current_screen.ids.img.source = self.download_image()
+
+
+class RootWidget(ScreenManager):
+    pass
+
+class MainApp(App):
+
+    def build(self):
+        return RootWidget()
+
+MainApp().run()
